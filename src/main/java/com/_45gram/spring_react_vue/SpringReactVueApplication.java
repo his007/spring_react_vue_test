c396f26668269723d696cd0c1a7f58f27c4fad22
hs <hwanginsub38@gmail.com>
2024-11-27 09:14:44 +0900

    #start

---
+++ src/main/java/com/_45gram/spring_react_vue/SpringReactVueApplication.java
@@ -0,0 +1,13 @@
+package com._45gram.spring_react_vue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactVueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactVueApplication.class, args);
+    }
+
+}
